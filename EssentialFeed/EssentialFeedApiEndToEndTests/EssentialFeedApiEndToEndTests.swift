d216fbf355bb3cea880a04154bf4bcc1f4de1851
David <69691740+piroworkz@users.noreply.github.com>
2025-09-21 19:16:13 -0600

    Add memory leak tracking for components in integration

--- EssentialFeed/EssentialFeedApiEndToEndTests/EssentialFeedApiEndToEndTests.swift
+++ EssentialFeed/EssentialFeedApiEndToEndTests/EssentialFeedApiEndToEndTests.swift
@@ -34,10 +34,12 @@ final class EssentialFeedApiEndToEndTests: XCTestCase {
 
 extension EssentialFeedApiEndToEndTests {
     
-    private func getFeedResult() -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        trackMemoryLeak(for: client, file: file, line: line)
+        trackMemoryLeak(for: loader, file: file, line: line)
         
         let exp = expectation(description: "Wait for load completion")
         
--- EssentialFeed/EssentialFeedTests/FeedApi/Utils/XCTestCase+MemoryLeakTracker.swift
+++ EssentialFeed/EssentialFeedTests/FeedApi/Utils/XCTestCase+MemoryLeakTracker.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 extension XCTestCase {
-    func trackMemoryLeak(for instance: AnyObject,  file: StaticString = #filePath, line: UInt = #line) {
+    func trackMemoryLeak(for instance: AnyObject, file: StaticString = #filePath, line: UInt = #line) {
         addTeardownBlock { [weak instance] in
             XCTAssertNil(instance, "Instance of SUT must be deallocated after each test", file: file, line: line)
         }
