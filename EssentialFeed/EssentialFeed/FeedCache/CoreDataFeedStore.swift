1c0f9012f7bad48d6010d752bfb6c8c53a66d47f
David <69691740+piroworkz@users.noreply.github.com>
2025-10-01 20:26:49 -0600

    Add ManagedCache and ManagedFeedImage model representations

--- EssentialFeed/EssentialFeed/FeedCache/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/CoreDataFeedStore.swift
@@ -5,7 +5,7 @@
 //  Created by David Luna on 27/09/25.
 //
 
-import Foundation
+import CoreData
 
 public class CoreDataFeedStore: FeedStore {
     
@@ -22,4 +22,17 @@ public class CoreDataFeedStore: FeedStore {
     public func retrieve(completion: @escaping RetrievalCompletion) {
         completion(.empty)
     }
+}
+
+private class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+private class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var url: URL
+    @NSManaged var descriptionText: String?
+    @NSManaged var location: String?
+    @NSManaged var cache: ManagedCache
 }
