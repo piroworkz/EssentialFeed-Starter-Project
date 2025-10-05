a08b3af39ca198d342eb627fce93af3890c4b692
David <69691740+piroworkz@users.noreply.github.com>
2025-10-05 09:57:57 -0600

    Add Prototype iOS app
    Added a new Prototype iOS app with initial files, including project configuration, assets, storyboards, and basic app delegate, scene delegate, and view controller implementations.

---
+++ Prototype/Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by David Luna on 05/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
