c8356026b43b214f6367b89713719e67e4b0992f
David <69691740+piroworkz@users.noreply.github.com>
2025-10-16 17:07:54 -0600

    Move UIButton test helper to separate file
    Extracted the UIButton simulateTap() test helper from FeedViewControllerTests.swift into a new Utils/UIButton+TestHelpers.swift file for better code organization and reuse.

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -468,15 +468,3 @@ private extension UIRefreshControl {
         }
     }
 }
-
-
-
-private extension UIButton {
-    func simulateTap() {
-        allTargets.forEach { target in
-            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
-                (target as NSObject).perform(Selector($0))
-            }
-        }
-    }
-}
