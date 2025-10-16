2c6f9d7ba6da579e14768098b692ad1b4538c079
David <69691740+piroworkz@users.noreply.github.com>
2025-10-16 17:18:26 -0600

    Move FeedImageCell test helpers to separate file
    Extracted FeedImageCell test helper extensions from FeedViewControllerTests.swift into a new Utils/FeedImageCell+TestHelpers.swift file to improve code organization and maintainability.

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -339,34 +339,3 @@ extension FeedViewControllerTests {
         
     }
 }
-
-private extension FeedImageCell {
-    
-    var isShowingLocation: Bool {
-        return !locationContainer.isHidden
-    }
-    
-    var locationText: String? {
-        return locationLabel.text
-    }
-    
-    var descriptionText: String? {
-        return descriptionLabel.text
-    }
-    
-    var isShowingImageLoadingIndicator: Bool {
-        return feedImageContainer.isShimmering
-    }
-    
-    var renderedImage: Data? {
-        return feedImageView.image?.pngData()
-    }
-    
-    var isShowingRetryAction: Bool {
-        return !feedImageRetryButton.isHidden
-    }
-    
-    func simulateRetryAction() {
-        feedImageRetryButton.simulateTap()
-    }
-}
