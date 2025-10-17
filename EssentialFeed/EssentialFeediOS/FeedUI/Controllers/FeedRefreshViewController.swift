7a6b2fa6311f20378f5ad3840ad1f33f74839d31
David <69691740+piroworkz@users.noreply.github.com>
2025-10-16 20:09:53 -0600

    Introduce FeedViewModel to decouple feed loading UI
    Added FeedViewModel to manage feed loading state and callbacks, refactored FeedUIComposer and FeedRefreshViewController to use the new view model. This improves separation of concerns between UI and data loading logic.

--- EssentialFeed/EssentialFeediOS/FeedUI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Controllers/FeedRefreshViewController.swift
@@ -6,31 +6,30 @@
 //
 
 import UIKit
-import EssentialFeed
 
 public final class FeedRefreshViewController: NSObject {
     
-    public lazy var view: UIRefreshControl = {
-        let view = UIRefreshControl()
-        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
-        return view
-    }()
-    private let feedLoader: FeedLoader
-    var onRefresh: (([FeedImage]) -> Void)?
+    private let viewModel: FeedViewModel
     
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
+    public lazy var view: UIRefreshControl = bind(UIRefreshControl())
+    
+    init(viewModel: FeedViewModel) {
+        self.viewModel = viewModel
+    }
+
+    @objc func refresh() {
+        viewModel.loadFeed()
     }
     
-
-    
-    @objc func refresh() {
-        view.beginRefreshing()
-        feedLoader.load { [weak self] result in
-            if let feed = try? result.get() {
-                self?.onRefresh?(feed)
+    func bind(_ view: UIRefreshControl) -> UIRefreshControl {
+        viewModel.onChange = { [weak self] viewModel in
+            if viewModel.isLoading {
+                self?.view.beginRefreshing()
+            } else {
+                self?.view.endRefreshing()
             }
-            self?.view.endRefreshing()
         }
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
     }
 }
