c2a9fd623da48c038458f00b202d9d131a977b7a
David <69691740+piroworkz@users.noreply.github.com>
2025-10-27 19:37:26 -0600

    Make URLProtocolStub thread-safe and refactor stubbing
    Introduces a serial dispatch queue to synchronize access to the stub in URLProtocolStub, ensuring thread safety. Refactors request observation to be part of the stub and removes the separate requestObserver variable. This improves reliability in concurrent test scenarios.

--- EssentialFeed/EssentialFeedTests/FeedApi/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedApi/URLSessionHTTPClientTests.swift
@@ -146,25 +146,32 @@ extension URLSessionHTTPClientTests {
     }
     
     private class URLProtocolStub: URLProtocol {
-        private static var stub: Stub?
-        private static var requestObserver: ((URLRequest) -> Void)?
-        
         private struct Stub{
             let data: Data?
             let response: URLResponse?
             let error: Error?
+            let requestObserver: ((URLRequest) -> Void)?
+        }
+        
+        private static let queue = DispatchQueue(label: "URLProtocolStub.queue")
+        
+        private static var _stub: Stub?
+        private static var stub: Stub? {
+            get { return queue.sync { _stub } }
+            set { queue.sync { _stub = newValue } }
         }
         
         static func stub(data: Data?, response: URLResponse?,  error: Error?) {
             stub = Stub(
                 data: data,
                 response: response,
-                error: error
+                error: error,
+                requestObserver: nil
             )
         }
         
         static func observeRequests(_ observer: @escaping (URLRequest) -> Void) {
-            requestObserver = observer
+            stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
         }
         
         static func startIntercepting() {
@@ -174,7 +181,6 @@ extension URLSessionHTTPClientTests {
         static func stopIntercepting() {
             URLProtocol.unregisterClass(URLProtocolStub.self)
             stub = nil
-            requestObserver = nil
         }
         
         override class func canInit(with request: URLRequest) -> Bool {
@@ -186,26 +192,19 @@ extension URLSessionHTTPClientTests {
         }
         
         override func startLoading() {
-            if let requestObserver = URLProtocolStub.requestObserver {
-                client?.urlProtocolDidFinishLoading(self)
-                return requestObserver(request)
-            }
-            
             guard let stub = URLProtocolStub.stub else { return }
-            
             if let data = stub.data {
                 client?.urlProtocol(self, didLoad: data)
             }
-            
             if let response = stub.response {
                 client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
             }
-            
             if let error = stub.error {
                 client?.urlProtocol(self, didFailWithError: error)
             } else {
                 client?.urlProtocolDidFinishLoading(self)
             }
+            stub.requestObserver?(request)
         }
         
         override func stopLoading() { }
