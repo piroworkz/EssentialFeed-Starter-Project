56d8ebcf3ec5ce70720177b342cadb7c5ee760cd
David <69691740+piroworkz@users.noreply.github.com>
2025-10-29 15:43:22 -0600

    Move FeedStore conformance to extension
    Refactored CoreDataFeedStore to remove FeedStore protocol conformance from the main class and implement it in a new extension file. This improves separation of concerns and keeps protocol-specific logic isolated.

---
+++ EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataFeedStore+FeedStore.swift
+//  EssentialFeed
+//
+//  Created by David Luna on 29/10/25.
+//
+
+import CoreData
+
+extension CoreDataFeedStore: FeedStore {
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        perform { context in
+            completion(Result {
+                let managedCache = try ManagedCache.newUniqueInstance(in: context)
+                managedCache.timestamp = timestamp
+                managedCache.feed = ManagedFeedImage.toManaged(feed, in: context)
+                try context.save()
+            })
+        }
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        perform { context in
+            completion(Result {
+                try ManagedCache.find(in: context).map { return CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp) }
+            })
+        }
+    }
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        perform { context in
+            completion(Result { try ManagedCache.find(in: context).map(context.delete).map(context.save) })
+        }
+    }
+}
