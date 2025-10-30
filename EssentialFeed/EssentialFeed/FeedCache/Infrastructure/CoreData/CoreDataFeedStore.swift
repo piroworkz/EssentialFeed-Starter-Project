70829db9cfb3720131ee622392d6b510f2bd7eb9
David <69691740+piroworkz@users.noreply.github.com>
2025-10-29 19:00:46 -0600

    Refactor CoreDataFeedStore error handling and model loading
    CoreDataFeedStore now uses a static model property and a custom StoreError enum for improved error handling. NSPersistentContainer.load now requires an explicit NSManagedObjectModel and throws standard errors instead of a custom enum. NSManagedObjectModel.with is now public for easier access.

--- EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
+++ EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore.swift
@@ -7,13 +7,21 @@
 import CoreData
 
 public class CoreDataFeedStore {
+    private static let modelName = "FeedStore"
+    private static let model = NSManagedObjectModel.with(name: modelName, in: Bundle(for: CoreDataFeedStore.self))
     private let container: NSPersistentContainer
     private let context: NSManagedObjectContext
     
     public init(storeURL: URL) throws {
-        let bundle = Bundle(for: CoreDataFeedStore.self)
-        container = try NSPersistentContainer.load(modelName: "FeedStore", url: storeURL, in: bundle)
-        context = container.newBackgroundContext()
+        guard let model = CoreDataFeedStore.model else { throw StoreError.modelNotFound }
+        
+        do {
+            container = try NSPersistentContainer.load(modelName: CoreDataFeedStore.modelName, model: model, url: storeURL)
+            context = container.newBackgroundContext()
+        } catch {
+            throw StoreError.failedToLoadPersistentStore(error)
+        }
+       
     }
     
     func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
@@ -29,4 +37,9 @@ public class CoreDataFeedStore {
     }
     
     deinit { cleanup() }
+    
+    enum StoreError: Error {
+        case modelNotFound
+        case failedToLoadPersistentStore(Error)
+    }
 }
--- EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataUtils.swift
+++ EssentialFeed/EssentialFeed/FeedCache/Infrastructure/CoreData/CoreDataUtils.swift
@@ -7,32 +7,20 @@
 
 import CoreData
 
- extension NSPersistentContainer {
-    enum LoadingError: Error {
-        case modelNotFound
-        case failedToLoadPersistentStore(Swift.Error)
-    }
+extension NSPersistentContainer {
     
-    static func load(modelName: String, url: URL, in bundle: Bundle) throws -> NSPersistentContainer {
-        guard let model = NSManagedObjectModel.with(name: modelName, in: bundle) else {
-            throw LoadingError.modelNotFound
-        }
-        
+    static func load(modelName: String, model: NSManagedObjectModel, url: URL) throws -> NSPersistentContainer {
         let description = NSPersistentStoreDescription(url: url)
         let container = NSPersistentContainer(name: modelName, managedObjectModel: model)
-        
         container.persistentStoreDescriptions = [description]
-        
         var loadError: Swift.Error?
         container.loadPersistentStores { loadError = $1 }
-        
-        try loadError.map { throw LoadingError.failedToLoadPersistentStore($0) }
-        
+        try loadError.map { throw $0 }
         return container
     }
 }
 
-private extension NSManagedObjectModel {
+extension NSManagedObjectModel {
     static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
         return bundle
             .url(forResource: name, withExtension: "momd")
