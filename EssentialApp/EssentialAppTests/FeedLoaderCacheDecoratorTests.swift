ba19bc24c448b2eb1bded41c85960f4f8329c2a6
David <69691740+piroworkz@users.noreply.github.com>
2025-10-31 11:17:24 -0600

    Add caching to FeedLoaderCacheDecorator and update tests
    Introduced a FeedCache protocol and updated FeedLoaderCacheDecorator to cache loaded feeds on success. Refactored tests to verify caching behavior and moved utility functions outside of FeedLoaderXCTCase extension for broader accessibility.

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -8,15 +8,26 @@
 import XCTest
 import EssentialFeed
 
+protocol FeedCache {
+    typealias Result = Swift.Result<Void, Error>
+    
+    func save(_ feed: [FeedImage], completion: @escaping (Result) -> Void)
+}
+
 class FeedLoaderCacheDecorator: FeedLoader {
     private let decoratee: FeedLoader
+    private let cache: FeedCache
     
-    init(decoratee: FeedLoader) {
+    init(decoratee: FeedLoader, cache: FeedCache) {
         self.decoratee = decoratee
+        self.cache = cache
     }
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load(completion: completion)
+        decoratee.load { [weak self] result in
+            self?.cache.save((try? result.get()) ?? []) { _ in }
+            completion(result)
+        }
     }
 }
 
@@ -24,16 +35,48 @@ final class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderXCTCase {
     
     func test_load_deliversFeedOnLoaderSuccess() {
         let feed = uniqueFeed()
-        let loader = FeedLoaderStub(result: .success(feed))
-        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        let sut = buildSUT(.success(feed))
         
         expect(sut, toCompleteWith: .success(feed))
     }
     
     func test_load_deliversErrorOnLoaderFailure() {
-        let loader = FeedLoaderStub(result: .failure(anyNSError()))
-        let sut = FeedLoaderCacheDecorator(decoratee: loader)
+        let sut = buildSUT(.failure(anyNSError()))
         
         expect(sut, toCompleteWith: .failure(anyNSError()))
+    }
+    
+    private func test_load_cachesLoadedFeedOnLoaderSuccess() {
+        let cache = CacheSpy()
+        let feed = uniqueFeed()
+        let sut = buildSUT(.success(feed), cache: cache)
+        
+        sut.load { _ in }
+        
+        XCTAssertEqual(cache.messages, [.save(feed)], "Expected to cache loaded feed on loader success")
+    }
+}
+
+extension FeedLoaderCacheDecoratorTests {
+    private func buildSUT(_ result: FeedLoader.Result, cache: CacheSpy = .init(), file: StaticString = #file, line: UInt = #line) -> FeedLoaderCacheDecorator {
+        let loader = FeedLoaderStub(result: result)
+        let sut = FeedLoaderCacheDecorator(decoratee: loader, cache: cache)
+        trackMemoryLeak(for: cache, file: file, line: line)
+        trackMemoryLeak(for: loader, file: file, line: line)
+        trackMemoryLeak(for: sut, file: file, line: line)
+        return sut
+    }
+    
+    private class CacheSpy: FeedCache {
+        private(set) var messages = [Message]()
+        
+        enum Message: Equatable {
+            case save([FeedImage])
+        }
+        
+        func save(_ feed: [FeedImage], completion: @escaping (FeedCache.Result) -> Void) {
+            messages.append(.save(feed))
+            completion(.success(()))
+        }
     }
 }
--- EssentialApp/EssentialAppTests/utils/EssentialAppTests+Utils.swift
+++ EssentialApp/EssentialAppTests/utils/EssentialAppTests+Utils.swift
@@ -33,21 +33,21 @@ extension FeedLoaderXCTCase where Self: XCTestCase {
             XCTAssertNil(instance, "Instance of SUT must be deallocated after each test", file: file, line: line)
         }
     }
-    
-    func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 0)
-    }
-    
-    func anyData() -> Data {
-        return Data("any data".utf8)
-    }
-    
-    func anyURL() -> URL {
-        return URL(string: "http://any-url.com")!
-    }
-    
-    func uniqueFeed() -> [FeedImage] {
-        return [FeedImage(id: UUID(), description: "any", location: "any", imageURL: URL(string: "http://any-url.com")!)]
-    }
-    
+}
+
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+func anyData() -> Data {
+    return Data("any data".utf8)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+func uniqueFeed() -> [FeedImage] {
+    return [FeedImage(id: UUID(), description: "any", location: "any", imageURL: URL(string: "http://any-url.com")!)]
 }
