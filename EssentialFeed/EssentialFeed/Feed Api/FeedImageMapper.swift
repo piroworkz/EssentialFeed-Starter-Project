959282486c7509cefd95a2d66c5b93fe4ddc13a1
David <69691740+piroworkz@users.noreply.github.com>
2025-11-08 16:53:35 -0600

    Replace LoadFeedFromRemoteUseCaseTests with FeedImageMapperTests
    Removed LoadFeedFromRemoteUseCaseTests and added FeedImageMapperTests to focus on testing FeedImageMapper directly. Updated project references and made FeedImageMapper public for test accessibility.

--- EssentialFeed/EssentialFeed/Feed Api/FeedImageMapper.swift
+++ EssentialFeed/EssentialFeed/Feed Api/FeedImageMapper.swift
@@ -7,9 +7,8 @@
 
 import Foundation
 
-class FeedImageMapper {
-    
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
+public class FeedImageMapper {
+    public static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedImage] {
         guard response.isOK, let items = try? JSONDecoder().decode(RemoteResponse.self, from: data).feedItems  else {
             throw RemoteFeedLoader.Error.invalidData
         }
