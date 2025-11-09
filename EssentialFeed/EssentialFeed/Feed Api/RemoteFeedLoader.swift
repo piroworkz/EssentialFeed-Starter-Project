965132dacb1f81ea518e9491a89cc45c6ae2bba5
David <69691740+piroworkz@users.noreply.github.com>
2025-11-08 19:02:18 -0600

    Remove unused Swift files and update .DS_Store
    Deleted RemoteFeedLoader.swift and HTTPURLResponse+Validation.swift from their respective modules, likely as part of a cleanup of unused code. Updated and added .DS_Store files, which are system files and not relevant to the codebase.

--- EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RemoteFeedLoader.swift
-//  EssentialFeed
-//
-//  Created by David Luna on 18/09/25.
-//
-
-import Foundation
--- EssentialFeed/EssentialFeed/ImageCommentsApi/HTTPURLResponse+Validation.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  EssentialFeed
-//
-//  Created by David Luna on 08/11/25.
-//
-import Foundation
-
